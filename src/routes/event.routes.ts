ea36bb597783975273185c90f55a0f805a3de965
Maruf Billah <marufbillah.dev@gmail.com>
2026-08-09 23:39:42 +0600

    feat(event): enhance event routes with role-based authorization for CRUD operations

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,16 +1,52 @@
 import { Router } from "express";
 import * as eventController from "../controllers/event.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
+import authorize from "../middlewares/role.middleware.js";
 
 const router = Router();
 
-router.get("/", authenticate, eventController.getEvents);
+router.get(
+  "/",
+  authenticate,
+  authorize("USER", "ADMIN", "ORGANIZER"),
+  eventController.getEvents,
+);
 router.get("/active", eventController.getActiveEvents);
-router.post("/", authenticate, eventController.createEvent);
-router.patch("/:id", authenticate, eventController.updateEvent);
-router.patch("/publish/:id", authenticate, eventController.publishEvent);
-router.patch("/cancel/:id", authenticate, eventController.cancelEvent);
-router.patch("/soft-delete/:id", authenticate, eventController.softDeleteEvent);
-router.patch("/restore/:id", authenticate, eventController.restoreEvent);
+router.post(
+  "/",
+  authenticate,
+  authorize("ORGANIZER"),
+  eventController.createEvent,
+);
+router.patch(
+  "/:id",
+  authenticate,
+  authorize("ORGANIZER", "ADMIN"),
+  eventController.updateEvent,
+);
+router.patch(
+  "/publish/:id",
+  authenticate,
+  authorize("ORGANIZER"),
+  eventController.publishEvent,
+);
+router.patch(
+  "/cancel/:id",
+  authenticate,
+  authorize("ORGANIZER"),
+  eventController.cancelEvent,
+);
+router.patch(
+  "/soft-delete/:id",
+  authenticate,
+  authorize("ADMIN"),
+  eventController.softDeleteEvent,
+);
+router.patch(
+  "/restore/:id",
+  authenticate,
+  authorize("ADMIN"),
+  eventController.restoreEvent,
+);
 
 export default router;
